5405086fade1bc4da5eec5cf5049ab78499a6db6
KarinaGuillen <60311191+KarinaGuillen@users.noreply.github.com>
2022-09-22 15:47:05 -0500

    Update calidad_del_aire.py
    Se agrega una gráfica más

--- calidad_del_aire.py
+++ calidad_del_aire.py
@@ -29,6 +29,14 @@ months_name = ['null', 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
 month_aqi_df['year-month'] = month_aqi_df.apply(lambda row: f'{int(row["year"]) % 2000}/{months_name[int(row["month"])]}', axis = 1)
 month_aqi_df['month-name'] = month_aqi_df.apply(lambda row: f'{months_name[int(row["month"])]}', axis = 1)
 
+
+counts = pd.value_counts(data_air['aqi'])
+df2= counts.to_frame()
+df3=df2
+df3['percent'] = (df2['aqi'] / df2['aqi'].sum()) * 100
+df3['index'] = df3.index
+
+
 # ---------- Dashboard ----------
 
 app = Dash(__name__)
@@ -58,6 +66,22 @@ layout2 = dict(
     showlegend=True,
     xaxis=dict(title="Mes"),
     yaxis=dict(title="AQI (Indice de calidad del aire)"),
+    margin=dict(l=40, r=0, t=40, b=30)
+)
+
+
+# Componente 3: #Para porcentajes
+data3 = dict(
+    x=df3["index"],
+    y=df3["percent"],
+    name="Porcentajes",
+    
+)
+
+layout3 = dict(
+    title="Porcentajes del puntaje de calidad del aire",
+    xaxis=dict(title="Puntaje calidad del aire"),
+    yaxis=dict(title="Porcentaje"),
     margin=dict(l=40, r=0, t=40, b=30)
 )
 
@@ -96,138 +120,31 @@ app.layout = html.Div([
                         color="red"
                     )
                 )
+                
             ],
             layout=layout2
         ),
+        
         style={
             'height': 300, 
             #'width': 1300,
             'margin': 'auto'
         }
-    )
+    ),
+       dcc.Graph(
+        id='aqi-percent',
+        figure=dict(
+            data=[data3],
+            layout=layout3
+        ),
+        style={
+            'height': 300, 
+            'width': 1300,
+            'margin': 'auto'
+        }
+    ),
 ])
 
 #Sentencias para abrir el servidor al ejecutar este script
 if __name__ == '__main__':
     app.run_server(port=8000, debug=True)
-
-# app = dash.Dash(__name__)
-#
-# df = pd.read_csv('ejemplo.csv')
-# #Objetos plotly.graph
-# data1 = [go.Scatter(x=df["co"],
-#                     y=df["aqi"],
-#                     mode="lines")]
-#
-# layout1 = go.Layout(title="prueba",
-#                     xaxis=dict(title="co"),
-#                     yaxis=dict(title="aqi"))
-#
-# data2 = [go.Bar(x=df["aqi"],
-#                     y=df["co"])]
-#
-# layout2 = go.Layout(title="preuba2",
-#                     xaxis=dict(title="aqi"),
-#                     yaxis=dict(title="co"))
-#
-# #Definición del layout de la app a partir de componentes HTML y Core
-# app.layout = html.Div([
-#                     dcc.Graph(id='lineplot', #Creación componente Graph 1
-#                     figure = {'data':data1,
-#                             'layout':layout1}
-#                     ),
-#                     dcc.Graph(id='barplot', #Creación componente Graph 2
-#                     figure = {'data':data2,
-#                             'layout':layout2}
-#                                     )])
-#
-# #Sentencias para abrir el servidor al ejecutar este script
-# if __name__ == '__main__':
-#     app.run_server(port=8000)
-
-
-# Mostramos el contenido
-
-# # Se verifica si hay la existencia de valores faltantes
-# data_air.isnull().sum().to_frame("Count_null")
-#
-# # Descripción de los datos
-# data_air.describe()
-#
-# # Información de los datos
-# data_air.info()
-#
-# # Librerías
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# plt.style.use('ggplot')
-#
-# #Correlaciones
-# print("====== Tabla - Correlación de las variables ======\n")
-# print(data_air.iloc[:,1:12].corr(), "\n"*2)
-#
-# # Matriz de correlación:
-# corrmat = data_air.iloc[:,1:12].corr()
-#
-# # Gráfica
-# plt.figure(figsize = (12, 9))
-# sns.heatmap(corrmat, vmax=.8, annot=True, square=True)
-#
-# plt.title("Mapa de calor (Correlación de las variables)", fontsize=16)
-# plt.show()
-#
-# # Tamaño de las gráficas
-# plt.figure(figsize = (16, 16))
-# cont = 0
-#
-# # Visualización de boxplots
-# for index in data_air:
-#
-#     if index != "id" and index != 'unix_measurement_dt':
-#         plt.subplot(4, 3, cont+1)
-#
-#         sns.boxplot(data = data_air, y = index, width = 0.2, linewidth = 1)
-#         plt.xlabel(index)
-#         plt.ylabel("")
-#         cont += 1
-#
-# plt.subplots_adjust(top= 0.96)
-# plt.suptitle("Gráficas de caja", fontsize=16)
-# plt.show()
-#
-# # Librería
-# import numpy as np
-#
-# # Dispersión de las variables
-# sns.pairplot(data=data_air.iloc[:,1:12], hue='aqi', vars=data_air.select_dtypes(include=np.number).columns[1:12])
-# plt.show()
-#
-# ########################################
-# #         ZONA EN CONSTRUCCIÓN         #
-# ########################################
-#
-# #########  HERRAMIENTAS #########
-# # División de datos de entrenamiento y de prueba
-# from sklearn.model_selection import train_test_split
-#
-# # Búsqueda de los mejores parámetros
-# from sklearn.model_selection import GridSearchCV
-#
-# ######## Modelos #########
-# # Vecinos más cercanos
-# from sklearn.neighbors import KNeighborsClassifier
-#
-# # Regresión logística
-# from sklearn.linear_model import LogisticRegression
-#
-# # Máquinas de soporte vectorial
-# from sklearn.svm import SVC
-#
-# # Árboles de decisión
-# from sklearn.tree import DecisionTreeClassifier
-#
-# # Árboles aleatorios
-# from sklearn.ensemble import RandomForestClassifier
-#
-# # Método de ensamble - AdaBoost
-# from sklearn.ensemble import AdaBoostClassifier
