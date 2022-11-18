f2c261156a69ccf9245b291c20529e3e42fa102d
Karina Guillen <170300067@ucaribe.edu.mx>
2022-11-18 12:54:37 -0600

    Se agregan graficas de pastel. Tambien predicciones de 2023 en las series de tiempo.

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ import psycopg2
 
 import datetime
 
+from pmdarima import auto_arima
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+
+
 load_figure_template('LUX')
 
 ###-------------    PREPROCESO
@@ -169,6 +174,115 @@ locations = data_air['full_location_name'].unique().tolist()
 conexion.close()
 print('\n---------Extraction: done----------')
 
+
+
+###--------Sección para dataframes con nuevos datos  
+
+###pronmedio ce calidad del aire en cancun
+data_cancun = data_air[ data_air['full_location_name'] == 'México, Quintana Roo, Cancún, Centro' ]
+counts_cancun = pd.value_counts(data_cancun['aqi'])
+df2= counts_cancun.to_frame()
+df3=df2
+df3['percent'] = (df2['aqi'] / df2['aqi'].sum()) * 100
+df3['index'] = df3.index
+label_cancun = ['1','2','3','4']
+fig_cancun = px.pie(values=df3['percent'], names= label_cancun, width=400, height=400, title='CANCUN', color=label_cancun, color_discrete_map={
+                                 '1':'#30A4DF',
+                                 '2':'#FC6ECD',
+                                 '3':'#616161',
+                                 '4':'#E1121E'})
+
+
+
+### promedio de calidad del aire en cdmx
+
+data_cdmx = data_air[ data_air['full_location_name'] == 'México, CDMX, CDMX, Ángel de la Independencia' ]
+counts_cdmx = pd.value_counts(data_cdmx['aqi'])
+df4= counts_cdmx.to_frame()
+df5=df4
+df5['percent'] = (df4['aqi'] / df4['aqi'].sum()) * 100
+df5['index'] = df5.index
+
+label_cdmx = ['4','5','1','2','3']
+fig_cdmx = px.pie(values=df5['percent'], names= label_cdmx, width=400, height=400 , title='CDMX', color=label_cdmx, color_discrete_map={
+                                  '4':'#E1121E',
+                                  '5':'#000000',
+                                 '1':'#30A4DF',
+                                 '2':'#FC6ECD',
+                                 '3':'#616161'
+                                 
+                                 })
+
+
+###--------------SECCION PAREA EL MODELO DE PREDICCION DE SERIES DE TIEMPO
+layout2 = dict(
+    title="Predicción de calidad del aire 2023",
+    showlegend=True,
+    xaxis=dict(title="Mes"),
+    yaxis=dict(title="AQI (Indice de calidad del aire)"),
+    margin=dict(l=40, r=0, t=40, b=30)
+)
+
+def serie_aqi_mes(data):
+  aqi_mean = data.groupby(
+    ['full_location_name', 'calendar_year', 'calendar_month'],
+    as_index = False)['aqi'].mean()
+
+  cancun = aqi_mean.loc[aqi_mean['full_location_name'] == 'México, Quintana Roo, Cancún, Centro']
+  cdmx = aqi_mean.loc[aqi_mean['full_location_name'] == 'México, CDMX, CDMX, Ángel de la Independencia']
+  cancun = cancun.iloc[:,2:4]
+  cdmx = cdmx.iloc[:, 2:4]
+  #cancun = cancun.set_index('calendar_month')
+  #cdmx= cdmx.set_index('calendar_month')
+  nueva_fila1 = {'calendar_month':11,'aqi':1.247777}
+  nueva_fila2 = {'calendar_month':12,'aqi':1.11112}
+  nueva_fila3 = {'calendar_month':11,'aqi':3.075402}
+  nueva_fila4 = {'calendar_month':12,'aqi':3.997656}
+  cancun=cancun.append(nueva_fila1, ignore_index=True) 
+  cancun=cancun.append(nueva_fila2, ignore_index=True) 
+  cdmx=cdmx.append(nueva_fila3, ignore_index=True) 
+  cdmx=cdmx.append(nueva_fila4, ignore_index=True) 
+  
+  return cancun, cdmx
+series_ind=serie_aqi_mes(data_air)
+
+###- cancun
+
+modelcn = SARIMAX(series_ind[0]['aqi'], #modelo con todos los datos de cancun 2021, 2022
+                order = (0,0,1), 
+                seasonal_order =(0,1,1,12))
+  
+resultcn = modelcn.fit()
+
+predictionscn = resultcn.predict(24, 35, #pedimos prediccion del año 2023 basandonos en los nuevos index que tendrian
+                             typ = 'levels').rename("Predictions")
+
+
+serie_predicha_cancun = pd.DataFrame()
+serie_predicha_cancun ['aqi']= predictionscn
+serie_predicha_cancun['mes'] = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+###cdmx
+
+modelmx = SARIMAX(series_ind[1]['aqi'], #modelo con todos los datos de cdmx 2021, 2022
+                order = (1,0,1), 
+                seasonal_order =(0,1,0,12))
+  
+resultmx = modelmx.fit()
+
+predictionsmx = resultmx.predict(24, 35, #pedimos prediccion del año 2023 basandonos en los nuevos index que tendrian
+                             typ = 'levels').rename("Predictions")
+
+serie_predicha_cdmx = pd.DataFrame()
+serie_predicha_cdmx ['aqi']= predictionsmx
+serie_predicha_cdmx['mes'] = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+
+
+
+
+
+
 ###--------------Build the figures / dropdowns------------------------------------
 
 x = np.random.sample(100)
@@ -184,7 +298,7 @@ SIDEBAR_STYLE = {
   'left': 0,
   'bottom': 0,
   'width': '24rem',
-  'height': '145vh',
+  'height': '380vh',
   'padding': '2rem 1rem',
   'background-color': '#A4CEE7',
 }
@@ -322,7 +436,77 @@ app.layout = html.Div(
                 'width': '90%',
                 'margin': 'auto'
               }
-            )
+            ),
+
+             html.P("."), 
+                                    
+            html.H3('PORCENTAJES DE AQI POR CIUDAD'),
+            dbc.Row([
+                                                dbc.Col(
+                                                      dcc.Graph(
+                                                      id='percents_cancun',
+                                                      figure=fig_cancun
+                                                      )
+                                                  ),
+                                                dbc.Col( 
+                                                   dcc.Graph(
+                                                  id='percents_cdmx',
+                                                  figure=fig_cdmx
+                                                  )
+                                                  ) 
+                                                ])
+
+                                  ,
+                                    html.P("."), 
+                                    html.H3('Predicción de calidad del aire 2023'),
+                                         dcc.Graph(
+                                          id='aqi_2023',
+                                          figure=dict(
+                                              data=[
+                                                  
+                                                  dict(
+                                                      x= serie_predicha_cancun ['mes'],
+                                                      y= serie_predicha_cancun ['aqi'],
+                                                      name="CANCUN",
+                                                      marker=dict(
+                                                          color="cyan"
+                                                      )
+                                                  )
+                                              ],
+                                              layout=layout2
+                                          ),
+                                          style={
+                                              'height': 300, 
+                                              'width': 1000,
+                                              'margin': 'auto'
+                                          }
+                                      ),
+                                      html.P("."), 
+                                       dcc.Graph(
+                                          id='aqi_2023_2',
+                                          figure=dict(
+                                              data=[
+                                                  
+                                                  dict(
+                                                      x= serie_predicha_cdmx ['mes'],
+                                                      y= serie_predicha_cdmx ['aqi'],
+                                                      name="CDMX",
+                                                      marker=dict(
+                                                          color="blue"
+                                                      )
+                                                  )
+                                              ],
+                                              layout=layout2
+                                          ),
+                                          style={
+                                              'height': 300, 
+                                              'width': 1000,
+                                              'margin': 'auto'
+                                          }
+                                      )
+
+
+
           ])
         ]))
       ])
